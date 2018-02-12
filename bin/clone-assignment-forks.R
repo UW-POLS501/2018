30c29af2044958ce1dd296ce4110cbbfdb03bce7
jrnold <jeffrey.arnold@gmail.com>
2018-02-12 11:45:03 -0800

    add notes on github tokens

--- bin/clone-assignment-forks.R
+++ bin/clone-assignment-forks.R
@@ -2,21 +2,22 @@
 #' Clone all forks or a repo (but meant for assignment)
 suppressPackageStartupMessages({
   library("purrr")
+  library("glue")
   library("gh")
   library("git2r")
 })
 
-#' Download all forks of a repo
+
+#' Download all forks of a GitHub repo
 #'
-#' For the GitHub organization \code{org}, for all members of
-#' the team \code{team}, download the their project directory, which is
-#' named \code{'org/:studentname'}.
+#' Clone all forks of the the repository \code{:owner/:repo} to a local
+#' path.
+#'
 #'
 #' @param repo string. Repository Name
 #' @param use_ssh flag. If \code{TRUE}, the use SSH to clone the directories,
 #'    else use HTTPS.
 #' @returns A list of \code{git_repository} objects
-#'
 clone_all_forks <- function(owner, repo, path = ".", use_ssh = TRUE) {
   forks <- gh("/repos/:owner/:repo/forks", owner = owner, repo = repo)
   # TODO: could filter forks by whether user is a student?
@@ -35,13 +36,16 @@ clone_all_forks <- function(owner, repo, path = ".", use_ssh = TRUE) {
       message(glue("Cloning {url} to {local_path}."))
       out[[url]] <- git2r::clone(url, local_path)
     } else {
+      # what to do if the local path already exists?
+      # if the path exists and is a git repo: warn but continue. this doesn't
+      # check that it is actually a fork of the original repo.
+      # if the path doesn't exist: stop
       warning(glue("WARNING: Not cloning {url} because {local_path}",
                    " already exists."))
       out[[url]] <- tryCatch({
         repository(local_path)
       }, error = function(e) {
-        print("ERROR: {local_path} is not a git repository")
-        NULL
+        stop("ERROR: {local_path} is not a git repository")
       })
     }
   }
